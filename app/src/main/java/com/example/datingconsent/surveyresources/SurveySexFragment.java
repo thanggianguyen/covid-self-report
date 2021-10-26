af53cb7790a2baa9b702c89282e15a43a8a86bff
ofitzelias <ejblaza@gmail.com>
2021-10-26 01:53:26 -0700

    worked on survey

--- app/src/main/java/com/example/datingconsent/surveyresources/SurveySexFragment.java
+++ app/src/main/java/com/example/datingconsent/surveyresources/SurveySexFragment.java
@@ -7,54 +7,49 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CheckBox;
+import android.widget.TextView;
 
 import com.example.datingconsent.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link SurveySexFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class SurveySexFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private CheckBox kiss, tongue;
+    private CheckBox bc, bc1, bc2, bc3, bc4, bc5, bc6;
+    private CheckBox bc7, bc8, bc9, bc10, bc11, bc12;
+    private TextView bcNote, bcSubnote;
 
     public SurveySexFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment SurveySexFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static SurveySexFragment newInstance(String param1, String param2) {
-        SurveySexFragment fragment = new SurveySexFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
+        /**
+         * Instantiate java parameters with UI parameters
+         */
+        kiss.findViewById(R.id.KissingCheck);
+        tongue.findViewById(R.id.TongueCheck);
+        bc.findViewById(R.id.BirthControlCheck);
+        bc1.findViewById(R.id.BirthControl1);
+        bc2.findViewById(R.id.BirthControl2);
+        bc3.findViewById(R.id.BirthControl3);
+        bc4.findViewById(R.id.BirthControl4);
+        bc5.findViewById(R.id.BirthControl5);
+        bc6.findViewById(R.id.BirthControl6);
+        bc7.findViewById(R.id.BirthControl7);
+        bc8.findViewById(R.id.BirthControl8);
+        bc9.findViewById(R.id.BirthControl9);
+        bc10.findViewById(R.id.BirthControl10);
+        bc11.findViewById(R.id.BirthControl11);
+        bc12.findViewById(R.id.BirthControl12);
+        bcNote.findViewById(R.id.BirthControlNote);
+        bcSubnote.findViewById(R.id.BirthControlSubnote);
+
     }
 
     @Override
@@ -62,5 +57,33 @@ public class SurveySexFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_survey_sex, container, false);
+
     }
+
+    //Sets visibility of parameters when CheckBox is clicked
+    public void KissingClicked(View v)
+    {
+        tongue.setVisibility(View.VISIBLE);
+    }
+
+    //Sets visibility of parameters when CheckBox is clicked
+    public void BirthControlClicked(View v)
+    {
+        bc1.setVisibility(View.VISIBLE);
+        bc2.setVisibility(View.VISIBLE);
+        bc3.setVisibility(View.VISIBLE);
+        bc4.setVisibility(View.VISIBLE);
+        bc5.setVisibility(View.VISIBLE);
+        bc6.setVisibility(View.VISIBLE);
+        bc7.setVisibility(View.VISIBLE);
+        bc8.setVisibility(View.VISIBLE);
+        bc9.setVisibility(View.VISIBLE);
+        bc10.setVisibility(View.VISIBLE);
+        bc11.setVisibility(View.VISIBLE);
+        bc12.setVisibility(View.VISIBLE);
+        bcNote.setVisibility(View.VISIBLE);
+        bcSubnote.setVisibility(View.VISIBLE);
+    }
+
+
 }
