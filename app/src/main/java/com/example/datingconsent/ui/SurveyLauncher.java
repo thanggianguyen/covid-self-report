55b4d83e671ab0f565902d83d57fd1e4b4182bb5
ofitzelias <ejblaza@gmail.com>
2022-01-11 15:54:39 -0800

    Tried modifying the SurveyLauncher to create the transition

--- app/src/main/java/com/example/datingconsent/ui/SurveyLauncher.java
+++ app/src/main/java/com/example/datingconsent/ui/SurveyLauncher.java
@@ -1,13 +1,17 @@
 package com.example.datingconsent.ui;
 
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import com.example.datingconsent.R;
 import com.example.datingconsent.surveyresources.*;
+
+import java.util.List;
 
 
 /**
@@ -21,7 +25,7 @@ public class SurveyLauncher extends AppCompatActivity {
     /** Fragment manager for this activity */
     private FragmentManager fm;
     /** The main Fragment to be displayed by this activity */
-    private Fragment mainFragment;
+    private Fragment datingFragment, sexFragment;
 
 
     /**
@@ -35,7 +39,46 @@ public class SurveyLauncher extends AppCompatActivity {
         setContentView(R.layout.activity_survey_launcher);
 
         fm = getSupportFragmentManager();
-        mainFragment = new SurveyDatingFragment();
-        fm.beginTransaction().replace(R.id.survey_content_container, mainFragment).commit();
+        datingFragment = new SurveyDatingFragment();
+        sexFragment = new SurveySexFragment();
+
+        FragmentTransaction transaction = fm.beginTransaction();
+        transaction.replace(R.id.survey_content_container, datingFragment);
+        transaction.commit();
+    }
+
+    public void datingDone(View view) {
+        FragmentTransaction transaction = fm.beginTransaction();
+        Fragment nextFragment = getNextFragment();
+        transaction.replace(R.id.survey_content_container, nextFragment);
+        transaction.commit();}
+    /**
+     * Returns the next question fragment to be displayed, if there is one, and calls for the
+     * response for the current fragment's question to be saved.
+     * If the currently displayed question fragment is Q2 (the final question), return null.
+     * If the currently displayed question fragment is not Q2, return the next question fragment.
+     * @return
+     */
+    private Fragment getNextFragment() {
+        Fragment current = getActiveFragment();
+        if (current instanceof SurveyDatingFragment)
+            return sexFragment;
+        else
+            return null;
+    }
+
+
+    /**
+     * Returns the current active question fragment within the fragment display:
+     * @return
+     */
+    private Fragment getActiveFragment() {
+        List<Fragment> fragments = fm.getFragments();
+        for (Fragment frag : fragments) {
+            if (frag != null && frag.isVisible())
+                return frag;
+        }
+        return null;
     }
 }
+
